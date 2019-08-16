915da47b8710ac1dfe1c581c72f6690459e3c020
Anton <your_email@whatever.com>
2019-08-16 11:36:09 +0300

    add project react-todo

---
+++ projects/react-todo/precache-manifest.dc8415190d046a281f80959382553402.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2e00978352271ddab53abbea534f893",
+    "url": "/projects/react-todo/index.html"
+  },
+  {
+    "revision": "028bcb6b7f1584f8fd53",
+    "url": "/projects/react-todo/static/css/main.06c5ad44.chunk.css"
+  },
+  {
+    "revision": "06f44d7c6eeee966ea35",
+    "url": "/projects/react-todo/static/js/2.34ad89f7.chunk.js"
+  },
+  {
+    "revision": "028bcb6b7f1584f8fd53",
+    "url": "/projects/react-todo/static/js/main.f63e32e4.chunk.js"
+  },
+  {
+    "revision": "8f1ed3a9047a0341d190",
+    "url": "/projects/react-todo/static/js/runtime~main.98ea7a07.js"
+  }
+]);
